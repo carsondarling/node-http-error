6b54c6eb97793a254516ef331955e505d228bd1a
Carson Darling <carsondarling@gmail.com>
2014-08-14 13:52:16 -0400

    added examples

---
+++ examples/express.js
@@ -0,0 +1,48 @@
+/**
+ * Note: This project does not include Express as dependency for a variety of
+ * reasons. If you want to run this example, make sure that you've installed
+ * express.
+ */
+
+var HTTPError = require('../');
+
+var express = require('express');
+var app = express();
+
+/**
+ * Route that generates an error
+ */
+app.get('/error', function(req, req, next) {
+  var shouldError = true;
+  if (shouldError) return next(new HTTPError(500, 'Error by design.'));
+});
+
+/**
+ * Error logging
+ */
+app.use(function(err, req, res, next) {
+  console.error(err);
+  next(err);
+});
+
+/**
+ * Error response
+ */
+app.use(function(err, req, res, next) {
+
+  var output = {error: {
+    message: err.message
+  }}
+
+  if (err instanceof HTTPError) {
+    res.status(err.code);
+    output.error.code = err.code;
+  }
+
+  res.send(output);
+});
+
+/**
+ * Start app
+ */
+app.listen(2000);
